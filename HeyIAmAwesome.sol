3e8c245cc814ac30e184b4eecf8ef3d87a410e08
john keru <keruk355@gmail.com>
2022-12-25 20:01:09 +0800

    first commit

---
+++ HeyIAmAwesome.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.6.6;
+
+contract HeyIAmAwesome {
+    function sayHello() public pure returns (string memory) {
+        return "Hello, I am keru hehe. The local repo is not yet finished and still working on it hehe.";
+    }
+}
